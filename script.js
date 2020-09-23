f6fb67999f9095a9aab5b2aeb477d1926afe0f76
Sumbul Kaynat <sumbulkaynat@gmail.com>
2020-09-23 16:22:14 +0530

    added style to the toggle button

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var id=document.getElementById("toggle");
 var text=document.querySelector("#text");
+var circle=document.querySelector("#circle");
 var count=0;
 id.addEventListener('click',function(){
     count++;
     if(count%2){
         document.body.style.backgroundColor = "black";
         text.style.color="white";
+        circle.style.marginLeft="101px";
+        circle.style.backgroundColor="white";
+        id.style.backgroundColor="black";
+        id.style.borderColor="white";
+    
     }else{
         document.body.style.backgroundColor = "white";
         text.style.color="black";
+        circle.style.marginLeft="3px";
+        circle.style.backgroundColor="black";
+        id.style.backgroundColor="white";
+        id.style.borderColor="black";
     }
     
 });
